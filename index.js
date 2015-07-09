c87bf6dad119e022e3c12df48068275fb1ecd229
Dan Kaplun <dbkaplun@gmail.com>
2015-07-09 00:30:04 -0400

    No more dependencies

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-var _ = require('lodash');
-
 function unsplay (items, key, parentKey) {
-  var nodes = _(items)
-    .indexBy(key)
-    .mapValues(function (item) { return {item: item, children: []}; })
-    .value();
+  var nodes = items.reduce(function (nodes, item) {
+    nodes[item[key]] = {item: item, children: []};
+    return nodes;
+  }, {});
+
   var lastCount, count;
   do {
     lastCount = count;
-    nodes = _.pick(nodes, function (node) {
-      var parentId = _.get(node.item, parentKey);
-      if (!(parentId in nodes)) return true;
-      _.get(nodes, parentId).children.push(node);
-    });
-    count = _.size(nodes);
+    count = 0;
+    nodes = Object.keys(nodes).reduce(function (newNodes, id) {
+      var node = nodes[id];
+      var parentId = node.item[parentKey];
+      if (parentId in nodes) nodes[parentId].children.push(node);
+      else {
+        newNodes[id] = node;
+        count++;
+      }
+      return newNodes;
+    }, {});
   } while (lastCount !== count);
-  return _.values(nodes);
+  return Object.keys(nodes).map(function (id) { return nodes[id]; });
 }
 
 module.exports = unsplay;
